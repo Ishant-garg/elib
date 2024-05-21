e352b1c3488589487680e009e3d49dd7d8560606
Ishant garg <ishantgarg2702014@gmail.com>
2024-05-21 19:52:03 +0530

    multer add

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,34 +1,70 @@
-import { NextFunction , Request ,Response} from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from 'bcrypt'; 
 import userModel from "./userModel";
 import createHttpError from "http-errors";
-import {sign} from 'jsonwebtoken'
+import { sign } from 'jsonwebtoken';
 import { config } from "../../config/config";
 
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, name, password } = req.body;
 
-const createUser = async(req:Request , res : Response ,next : NextFunction) =>{
-    const {email , name , password} = req.body;
-
-    if(!email || !name || !password){
-        res.send('All fields are required');
+    if (!email || !name || !password) {
+        return next(createHttpError(400, 'All fields are required'));
     }
-    //checking if user alerady exists or not in db
-    const user = await userModel.findOne({email});
-    if(user){
-        const error = createHttpError(400 , "User already exists");
-        return next(error);
-    } 
-    const hashedPassword = await bcrypt.hash(password , 10);
-   //adding user into database
-    const newUser = await userModel.create({
-        name,
-        email,
-        password : hashedPassword
-    })
-    //token generation
 
-    const token = sign({sub : newUser._id} , config.jwt_secret as string , {expiresIn : "7d"})
-    return res.json({accessToken : token});
-}
+    try {
+        // Checking if user already exists
+        const user = await userModel.findOne({ email });
+        if (user) {
+            return next(createHttpError(400, 'User already exists'));
+        }
 
-export {createUser}
+        // Hashing the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Adding user into database
+        const newUser = await userModel.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        // Token generation
+        const token = sign({ sub: newUser._id }, config.jwt_secret as string, { expiresIn: "7d" });
+
+        return res.status(201).json({ accessToken: token });
+    } catch (error) {
+        return next(createHttpError(500, 'Internal Server Error'));
+    }
+};
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(createHttpError(400, 'All fields are required.'));
+    }
+
+    try {
+        // Checking if user exists
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return next(createHttpError(404, 'User not found'));
+        }
+
+        // Checking if password matches
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return next(createHttpError(400, 'Email or password incorrect'));
+        }
+
+        // Token generation
+        const token = sign({ sub: user._id }, config.jwt_secret as string, { expiresIn: "7d" });
+
+        return res.status(200).json({ accessToken: token });
+    } catch (error) {
+        return next(createHttpError(500, 'Internal Server Error'));
+    }
+};
+
+export { createUser, loginUser };
